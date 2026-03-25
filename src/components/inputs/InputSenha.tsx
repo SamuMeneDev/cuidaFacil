cd5ade2fdd7e072c6f12fc5fb3f5872e8cfa4a4b
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-03-25 15:26:57 -0300

    Criando componentes utilitários

--- src/components/inputs/InputSenha.tsx
+++ src/components/inputs/InputSenha.tsx
@@ -1,13 +1,19 @@
-import { TextInput, TextInputProps, View } from "react-native";
+import globalStyles from "@/globalStyles";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
+import { TextInput, TextInputProps, View, Pressable } from "react-native";
 
-interface InputSenhaProps extends TextInputProps {
-
-}
+interface InputSenhaProps extends TextInputProps {}
 
 export default function InputSenha({...props}: InputSenhaProps) {
+    const [mostrarSenha, setMostrarSenha] = useState(false);
+
     return (
-        <View>
-            <TextInput {...props}  />
+        <View className="border flex-row justify-between items-center border-indigo-200 p-1 rounded-xl bg-indigo-100">
+            <TextInput secureTextEntry={!mostrarSenha} {...props} className="w-[90%]"  />
+            <Pressable onPress={()=>setMostrarSenha(!mostrarSenha)} className="flex items-center w-[10%]">
+                <Feather name={mostrarSenha?"eye-off":"eye"} color={globalStyles.colors.azul600} size={20} />
+            </Pressable>
         </View>
     )
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,6 +4,9 @@ import { Logo } from "@/components/Logo";
 import { Feather } from "@expo/vector-icons";
 import globalStyles from "@/globalStyles";
 import { CommonActions, useNavigation } from "@react-navigation/native";
+import InputSenha from "@/components/inputs/InputSenha";
+import InputDefault from "@/components/inputs/InputDefault";
+import TextButton from "@/components/buttons/TextButton";
 export function Login() {
     const navigation = useNavigation();
 
@@ -15,7 +18,7 @@ export function Login() {
                 </View>
                 {/* SAUDACOES */}
                 <View className="flex items-center gap-3">
-                    <Text className="font-semibold text-cyan-800 text-xl">Bem Vindo(a) de volta</Text>
+                    <Text className="font-semibold text-sky-900 text-xl">Bem Vindo(a) de volta</Text>
                     <Logo />
                     <Text className="text-cyan-800 font-normal text-xl">Faça Login</Text>
                 </View>
@@ -23,16 +26,9 @@ export function Login() {
                     {/*CAMPOS*/}
                     <View className="p-2 gap-3">
                         {/* EMAIL */}
-                        <View className="border flex-row border-slate-400 p-1 rounded-xl bg-slate-200">
-                            <TextInput placeholder="Insira seu e-mail" />
-                        </View>
+                        <InputDefault placeholder="Insira o seu email" />
                         {/* SENHA */}
-                        <View className="border flex-row justify-between items-center border-slate-400 p-1 rounded-xl bg-slate-200">
-                            <TextInput placeholder="Insira sua senha" className="w-[90%]" />
-                            <Pressable className="w-[10%]">
-                                <Feather name="eye" color={globalStyles.colors.azul600} size={24} />
-                            </Pressable>
-                        </View>
+                        <InputSenha placeholder="Teste" />
                     </View>
 
                     {/* NÃO TEM UMA CONTA */}
@@ -43,9 +39,7 @@ export function Login() {
                     </View>
                     {/* LOGAR */}
                     <View className="items-center">
-                        <Pressable className="min-w-[30%] flex items-center bg-cyan-600 border border-cyan-700 p-2 rounded-xl">
-                            <Text className="text-white text-xl">Logar</Text>
-                        </Pressable>
+                        <TextButton />
                     </View>
 
 
