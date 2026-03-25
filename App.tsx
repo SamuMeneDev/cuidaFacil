b8749ebeb6474a9fc0c72d7e0c2e087429434fcc
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-03-25 17:20:43 -0300

    Inicio tela de home

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Splash } from './src/pages/Splash/Splash';
 import { Login } from './src/pages/Login/Login';
 import Cadastro from '@/pages/Cadastro/Cadastro';
+import Home from '@/pages/Home/Home';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +12,8 @@ export default function App() {
   return (
    <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen component={Splash} name="Splash" options={{headerShown:false}} />
+      <Stack.Screen component={Home} name="Home" options={{headerShown:false, gestureEnabled: false}} />
+      <Stack.Screen component={Splash} name="Splash" options={{headerShown:false, gestureEnabled: false}} />
       <Stack.Screen component={Login} name="Login" options={{headerShown:false}} />
       <Stack.Screen component={Cadastro} name="Cadastro" options={{headerShown:false}} />
     </Stack.Navigator>
